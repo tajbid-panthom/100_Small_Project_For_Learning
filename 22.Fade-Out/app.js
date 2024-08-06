7b196a90430e19677981bb2168f7e691065fd710
tajbid-panthom <tajbidhossain903@gmail.com>
2024-08-06 23:06:06 +0600

    Fade Out No Scroll

---
+++ 22.Fade-Out/app.js
@@ -0,0 +1,9 @@
+let arrow = document.querySelector("#arrow")
+
+window.addEventListener("scroll", () => {
+    let position = window.scrollY
+    if (position <= 5) {
+        arrow.classList.toggle("fade-in")
+        arrow.classList.toggle("fade-out")
+    }
+})

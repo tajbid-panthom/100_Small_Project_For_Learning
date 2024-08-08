9646d69a88bb0eda023bae6c30651b7554277173
tajbid-panthom <tajbidhossain903@gmail.com>
2024-08-08 15:41:07 +0600

    Social Media Menu Select

---
+++ 29.Social-Media-Menu-Select/app.js
@@ -0,0 +1,19 @@
+let selectField = document.getElementById("selectField")
+let selectText = document.getElementById("selectText")
+let list = document.getElementById("list")
+let arrowIcon = document.getElementById("arrowIcon")
+let options = document.getElementsByClassName("options")
+
+
+selectField.addEventListener("click", () => {
+    list.classList.toggle("hide")
+    arrowIcon.classList.toggle("rotate")
+})
+
+for (let option of options) {
+    option.onclick = function () {
+        selectText.innerHTML = this.textContent
+        list.classList.toggle("hide")
+        arrowIcon.classList.toggle("rotate")
+    }
+};
